87f404d3b66bf14bc152dd94cca9e5c71f7b5438
trombillo <marcoramp12@gmail.com>
2024-12-13 11:54:42 +0000

    ..

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template("index.html", titolo=home)
+
+@app.route("/")
+def details():
+    prodotti=(("siero alla caffeina" , "bellezza", "20")("korean tone", "bellezza", "15")("gua sha", "bellezza", "11"))
+    
 
 app.run()
